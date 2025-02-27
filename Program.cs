9720b5d2893c232450b498385ede2d484ec014d6
Fallingstash <sashkaprog228@gmail.com>
2025-02-27 18:41:23 +0700

    create main class

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Document
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public string Keywords { get; set; }
+    public string Theme { get; set; }
+    public string FilePath { get; set; }
+
+    public virtual string GetInfo()
+    {
+        return $"Name: {Name}, Author: {Author}, Keywords: {Keywords}, Theme: {Theme}, FilePath: {FilePath}";
+    }
+}
